274b1eab59c4dd1a22245e47f9c8a11a4c2378d2
Yannick Widmer <yannick.widmer@math.uzh.ch>
2014-10-29 14:18:14 +0100

    file loader made Portable

--- Roman_29102014_0916/loadData.m
+++ Roman_29102014_0916/loadData.m
@@ -1,9 +1,9 @@
-%clear all
+clear all
 %close all
 
-path(path, '.\utils');
+path(path,strcat( '.',filesep,'utils'));
 
-fileNames = getFiles('.\data\cp')
+fileNames = getFiles(strcat('.',filesep,'data',filesep,'cp'))
 
 d_cp = []
 d_cpl = []
@@ -20,85 +20,82 @@ d_w = []
 d_wl = []
 d_wv = []
 
+
+fileNames = getFiles(strcat('.',filesep,'data',filesep,'cpl'))
+
 for kk = 1:length(fileNames)
-   d_cp = [d_cp;loadContentCP(['.\data\cp\',fileNames{kk}])]
+   d_cpl = [d_cpl;loadContentCPL([strcat('.',filesep,'data',filesep,'cpl',filesep),fileNames{kk}])]
 end
 
-fileNames = getFiles('.\data\cpl')
+fileNames = getFiles(strcat('.',filesep,'data',filesep,'cpv'))
 
 for kk = 1:length(fileNames)
-   d_cpl = [d_cpl;loadContentCPL(['.\data\cpl\',fileNames{kk}])]
+   d_cpv = [d_cpv;loadContentCPV([strcat('.',filesep,'data',filesep,'cpv',filesep),fileNames{kk}])]
 end
 
-fileNames = getFiles('.\data\cpv')
+fileNames = getFiles(strcat('.',filesep,'data',filesep,'cv'))
 
 for kk = 1:length(fileNames)
-   d_cpv = [d_cpv;loadContentCPV(['.\data\cpv\',fileNames{kk}])]
+   d_cv = [d_cv;loadContentCV([strcat('.',filesep,'data',filesep,'cv',filesep),fileNames{kk}])]
 end
 
-fileNames = getFiles('.\data\cv')
+fileNames = getFiles(strcat('.',filesep,'data',filesep,'dh'))
 
 for kk = 1:length(fileNames)
-   d_cv = [d_cv;loadContentCV(['.\data\cv\',fileNames{kk}])]
+   d_dh = [d_dh;loadContentDH([strcat('.',filesep,'data',filesep,'dh',filesep),fileNames{kk}])]
 end
 
-fileNames = getFiles('.\data\dh')
+fileNames = getFiles(strcat('.',filesep,'data',filesep,'dl'))
 
 for kk = 1:length(fileNames)
-   d_dh = [d_dh;loadContentDH(['.\data\dh\',fileNames{kk}])]
+   d_dl = [d_dl;loadContentDL([strcat('.',filesep,'data',filesep,'dl',filesep),fileNames{kk}])]
 end
 
-fileNames = getFiles('.\data\dl')
+fileNames = getFiles(strcat('.',filesep,'data',filesep,'dt'))
 
 for kk = 1:length(fileNames)
-   d_dl = [d_dl;loadContentDL(['.\data\dl\',fileNames{kk}])]
+   d_dt = [d_dt;loadContentDT([strcat('.',filesep,'data',filesep,'dt',filesep),fileNames{kk}])]
 end
 
-fileNames = getFiles('.\data\dt')
+fileNames = getFiles(strcat('.',filesep,'data',filesep,'dv'))
 
 for kk = 1:length(fileNames)
-   d_dt = [d_dt;loadContentDT(['.\data\dt\',fileNames{kk}])]
+   d_dv = [d_dv;loadContentDV([strcat('.',filesep,'data',filesep,'dv',filesep),fileNames{kk}])]
 end
 
-fileNames = getFiles('.\data\dv')
+fileNames = getFiles(strcat('.',filesep,'data',filesep,'jt'))
 
 for kk = 1:length(fileNames)
-   d_dv = [d_dv;loadContentDV(['.\data\dv\',fileNames{kk}])]
+   d_jt = [d_jt;loadContentJT([strcat('.',filesep,'data',filesep,'jt',filesep),fileNames{kk}])]
 end
 
-fileNames = getFiles('.\data\jt')
+fileNames = getFiles(strcat('.',filesep,'data',filesep,'p-rho-T'))
 
 for kk = 1:length(fileNames)
-   d_jt = [d_jt;loadContentJT(['.\data\jt\',fileNames{kk}])]
+   d_prhot = [d_prhot;loadContentPRHOT([strcat('.',filesep,'data',filesep,'p-rho-T',filesep),fileNames{kk}])]
 end
 
-fileNames = getFiles('.\data\p-rho-t')
+fileNames = getFiles(strcat('.',filesep,'data',filesep,'psigma'))
 
 for kk = 1:length(fileNames)
-   d_prhot = [d_prhot;loadContentPRHOT(['.\data\p-rho-t\',fileNames{kk}])]
+   d_psigma = [d_psigma;loadContentPSIGMA([strcat('.',filesep,'data',filesep,'psigma',filesep),fileNames{kk}])]
 end
 
-fileNames = getFiles('.\data\psigma')
+fileNames = getFiles(strcat('.',filesep,'data',filesep,'w'))
 
 for kk = 1:length(fileNames)
-   d_psigma = [d_psigma;loadContentPSIGMA(['.\data\psigma\',fileNames{kk}])]
-end
-
-fileNames = getFiles('.\data\w')
-
-for kk = 1:length(fileNames)
-   d_w = [d_w;loadContentW(['.\data\w\',fileNames{kk}])]
+   d_w = [d_w;loadContentW([strcat('.',filesep,'data',filesep,'w',filesep),fileNames{kk}])]
 end
 
 
-fileNames = getFiles('.\data\wl')
+fileNames = getFiles(strcat('.',filesep,'data',filesep,'wl'))
 
 for kk = 1:length(fileNames)
-   d_wl = [d_wl;loadContentWL(['.\data\wl\',fileNames{kk}])]
+   d_wl = [d_wl;loadContentWL([strcat('.',filesep,'data',filesep,'wl',filesep),fileNames{kk}])]
 end
 
-fileNames = getFiles('.\data\wv')
+fileNames = getFiles(strcat('.',filesep,'data',filesep,'wv'))
 
 for kk = 1:length(fileNames)
-   d_wv = [d_wv;loadContentWV(['.\data\wv\',fileNames{kk}])]
+   d_wv = [d_wv;loadContentWV([strcat('.',filesep,'data',filesep,'wv',filesep),fileNames{kk}])]
 end
